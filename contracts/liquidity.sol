21e9180802ef3077eceec109b59a59c4c821e97e
Rishabh Gupta <rishotics@gmail.com>
2022-09-11 16:47:15 +0530

    remove unot required modules

--- contracts/liquidity.sol
+++ contracts/liquidity.sol
@@ -256,59 +256,6 @@ contract LiquidityUniswapV3 is IERC721Receiver {
         (sqrtPrice, tick, , , , , ) = _pool.slot0();
     }
 
-    function increaseLiquidityCurrentRange(
-        uint256 amountAdd0,
-        uint256 amountAdd1
-    )
-        external
-        returns (
-            uint128 liquidity,
-            uint256 amount0,
-            uint256 amount1
-        )
-    {
-        TransferHelper.safeTransferFrom(
-            DAI,
-            msg.sender,
-            address(this),
-            amountAdd0
-        );
-        TransferHelper.safeTransferFrom(
-            USDC,
-            msg.sender,
-            address(this),
-            amountAdd1
-        );
-
-        TransferHelper.safeApprove(
-            DAI,
-            address(nonfungiblePositionManager),
-            amountAdd0
-        );
-        TransferHelper.safeApprove(
-            USDC,
-            address(nonfungiblePositionManager),
-            amountAdd1
-        );
-
-        INonfungiblePositionManager.IncreaseLiquidityParams
-            memory params = INonfungiblePositionManager
-                .IncreaseLiquidityParams({
-                    tokenId: tokenId,
-                    amount0Desired: amountAdd0,
-                    amount1Desired: amountAdd1,
-                    amount0Min: 0,
-                    amount1Min: 0,
-                    deadline: block.timestamp
-                });
-
-        (liquidity, amount0, amount1) = nonfungiblePositionManager
-            .increaseLiquidity(params);
-
-        console.log("liquidity", liquidity);
-        console.log("amount 0", amount0);
-        console.log("amount 1", amount1);
-    }
 
     function decreaseLiquidity(uint256 _tokenId)
         external
@@ -333,7 +280,7 @@ contract LiquidityUniswapV3 is IERC721Receiver {
 
         console.log("amount 0", amount0);
         console.log("amount 1", amount1);
-        console.log("collecting fees");
+        console.log("collecting fees...");
         collectAllFees(_tokenId);
 
         nonfungiblePositionManager.burn(_tokenId);
